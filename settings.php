3146b0c172a209d0f57b0b0b078bdfaa697716d7
Bogdan Zavadovschi <bogdanzavadovschi17@gmail.com>
2020-08-14 21:28:45 +0300

    A tweak to prepare settings page.

---
+++ settings.php
@@ -0,0 +1,73 @@
+<?php
+include(__DIR__.'/api/init.php');
+
+
+/**
+ * Checking if the application is installed.
+ */
+if(intval(GET_ENV['CORE_RUN_SCRIPT'])!==2){
+    $vars['header']['title'] = 'Error';
+    $msg = "Your service is not installed. Before doing anything please go and install it!";
+    $vars['error'] = array(
+        'name' => "Zephyrus is not installed",
+        'message' => $msg,
+        'url' => "/admin/install",
+        'button' => 'Install'
+    );
+    $template = new \Api\Misc\Render('/main/templates');
+    $template->render('pages/misc/error', $vars);
+    die("Service is not installed!");
+}
+
+/**
+ * Checking if the user is logged in
+ */
+if(!$session->checkLogin()){
+    header("Refresh:0; url=/presentation", true, 401);
+    die("You must be logged in to access this page!");
+}
+/**
+ * Get the logged user
+ */
+$user = \Api\Management\Users::find_by_attribute("uuid", $_SESSION['user']);
+
+/**
+ * Check if the user has enough permission
+ */
+
+// if(!$role->hasPermission($user, "readPosts")){
+//     header("Refresh:0; url=/", true, 401);
+//     die("Insufficient permissions!");
+// }
+
+
+
+
+/**
+ * This all must be reworked and set like it should be!
+ */
+
+
+$page='main';
+if(isset($_GET['page']) && !empty($_GET['page']))
+    $page = $_GET['page'];
+$path = "settings";
+$name = $page;
+$vars = array(
+    "header" => array('title'=>$name),
+    "body" =>array('name'=>strtolower($page)),
+    "user" => $user,
+);
+
+
+/**
+ * This must be removed once done all the implementation
+ */
+$values = array(
+    'author' => array(
+        'image' => 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fd/fd4372b86edca6468c80bfa8c79361c250fbb22c_full.jpg',
+    ),
+);
+$vars = array_merge($vars, $values);
+$template = new \Api\Misc\Render('/main/templates');
+$template->render("/pages".'/'.$path.'/'.strtolower($page), $vars);
